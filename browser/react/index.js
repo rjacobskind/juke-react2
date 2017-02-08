6aa2846d4210720d3508fe8f37330e988558aa9e
Christine <cleverett1@gmail.com>
2017-02-08 10:50:23 -0500

    up to 4

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import { Router, Route, hashHistory } from 'react-router';
 
-ReactDOM.render(
-  <AppContainer />,
-  document.getElementById('app')
+ReactDOM.render((
+  <Router history= {hashHistory} >
+    <Route path="/" component={AppContainer}>
+    </Route>
+  </Router>
+), document.getElementById('app')
 );
